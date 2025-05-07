7593f625b239de2852f744d26afc66650d537437
Cédrick-Armel YEBOUET <35418979+CedrickArmel@users.noreply.github.com>
2025-05-07 10:15:56 +0000

    fix: reproductibility

--- src/app/utils/utils.py
+++ src/app/utils/utils.py
@@ -86,39 +86,6 @@ def get_callbacks(cfg: "DictConfig") -> "tuple[Callback, ...]":
     return chckpt_cb, lr_cb
 
 
-def get_data_loader(
-    cfg: "DictConfig", df: "pd.DataFrame", mode: "str"
-) -> "DataLoader | tuple[DataLoader, ...] | None":
-    """
-    Creates and returns PyTorch DataLoader objects for training, validation, or testing
-    based on the provided configuration.
-    Args:
-        cfg (DictConfig): A configuration object containing the following attributes:
-            - train (bool): Whether to create DataLoaders for training and validation.
-            - test (bool): Whether to create a DataLoader for testing.
-            - batch_size (int): Batch size for training DataLoader.
-            - batch_size_val (int): Batch size for validation and test DataLoaders.
-            - num_workers (int): Number of worker threads for data loading.
-            - pin_memory (bool): Whether to pin memory for training DataLoader.
-            - val_pin_memory (bool): Whether to pin memory for validation DataLoader.
-            - test_pin_memory (bool): Whether to pin memory for test DataLoader.
-            - shuffle (bool): Whether to shuffle the training data.
-            - drop_last (bool): Whether to drop the last incomplete batch in training.
-            - prefetch_factor (int): Number of batches to prefetch.
-    Returns:
-        DataLoader
-    """
-    dataset = BYUCustomDataset(cfg, df=df, mode=mode)
-    loader = DataLoader(
-        dataset=dataset,
-        batch_size=cfg.batch_size if mode == "train" else cfg.batch_size_val,
-        collate_fn=collate_fn,
-        num_workers=cfg.num_workers if mode == "train" else cfg.num_val_workers,
-        shuffle=cfg.shuffle if mode == "train" else False,
-    )
-    return loader
-
-
 def get_data(cfg: "DictConfig", mode: "str" = "fit") -> "tuple[pd.DataFrame,...]":
     """
     Loads and splits a dataset into training and validation DataFrames based on the provided configuration.
@@ -152,6 +119,121 @@ def get_data(cfg: "DictConfig", mode: "str" = "fit") -> "tuple[pd.DataFrame,...]
         num_ids = list(range(0, len(test_tomo_id)))
         data = pd.DataFrame(dict(tomo_id=test_tomo_id, id=num_ids))
     return data
+
+
+def get_data_loader(
+    cfg: "DictConfig", df: "pd.DataFrame", mode: "str"
+) -> "DataLoader | tuple[DataLoader, ...] | None":
+    """
+    Creates and returns PyTorch DataLoader objects for training, validation, or testing
+    based on the provided configuration.
+    Args:
+        cfg (DictConfig): A configuration object containing the following attributes:
+            - train (bool): Whether to create DataLoaders for training and validation.
+            - test (bool): Whether to create a DataLoader for testing.
+            - batch_size (int): Batch size for training DataLoader.
+            - batch_size_val (int): Batch size for validation and test DataLoaders.
+            - num_workers (int): Number of worker threads for data loading.
+            - pin_memory (bool): Whether to pin memory for training DataLoader.
+            - val_pin_memory (bool): Whether to pin memory for validation DataLoader.
+            - test_pin_memory (bool): Whether to pin memory for test DataLoader.
+            - shuffle (bool): Whether to shuffle the training data.
+            - drop_last (bool): Whether to drop the last incomplete batch in training.
+            - prefetch_factor (int): Number of batches to prefetch.
+    Returns:
+        DataLoader
+    """
+    g = get_seeded_generator(cfg)
+    dataset = BYUCustomDataset(cfg, df=df, mode=mode)
+    loader = DataLoader(
+        dataset=dataset,
+        batch_size=cfg.batch_size if mode == "train" else cfg.batch_size_val,
+        collate_fn=collate_fn,
+        worker_init_fn=seed_worker,
+        num_workers=cfg.num_workers if mode == "train" else cfg.num_val_workers,
+        shuffle=cfg.shuffle if mode == "train" else False,
+        generator=g,
+    )
+    return loader
+
+
+def get_linear_schedule_with_warmup(
+    optimizer: "optim.Optimizer",
+    num_warmup_steps: "int",
+    num_training_steps: "int",
+    end_lambda: "float" = 0.0,
+    last_epoch: "int" = -1,
+) -> "optim.lr_scheduler.LambdaLR":
+    """
+    from https://github.com/huggingface/transformers/blob/main/src/transformers/optimization.py
+    Create a schedule with a learning rate that decreases linearly from the initial lr set in the optimizer to 0, after
+    a warmup period during which it increases linearly from 0 to the initial lr set in the optimizer.
+    Args:
+        optimizer ([`~torch.optim.Optimizer`]):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (`int`):
+            The total number of training steps.
+        end_lambda (`float`):
+            Mutiplicative factor at the end of training.
+        last_epoch (`int`, *optional*, defaults to -1):
+            The index of the last epoch when resuming training.
+    Return:
+        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    def lr_lambda(current_step: int) -> "float":
+        """returns the learning rate multiplier based on the current step."""
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        return max(
+            end_lambda,
+            float(num_training_steps - current_step)
+            / float(max(1, num_training_steps - num_warmup_steps)),
+        )
+
+    return optim.lr_scheduler.LambdaLR(optimizer, lr_lambda, last_epoch)
+
+
+def get_multistep_schedule_with_warmup(
+    optimizer: "optim.Optimizer",
+    warmup_steps: "int",
+    training_steps: "int",
+    end_lambda: "float" = 1e-3,
+    m: "int" = 10,
+    last_epoch: "int" = -1,
+) -> "optim.lr_scheduler.LambdaLR":
+    """
+    Modified schedule:
+      - Warmup: linearly increases from 0.001% of base LR to 100% of base LR over warmup_steps.
+      - Then, in the non-warmup phase (total_steps - warmup_steps):
+          * LR = base_lr * [(1- n/m) + end_lambda] where:
+              * m: number of milestones
+              * n: step number in [1, m]
+    """
+
+    def create_milestones(steps: "int", m: "int") -> "list[int]":
+        """returns a list of milestones for the given number of steps and m."""
+        g = int(steps // m)
+        milestones = []
+        for i in range(1, m + 1):
+            milestones += [i] * g
+        return milestones
+
+    def lr_lambda(current_step: int) -> "float":
+        """returns the learning rate multiplier based on the current step."""
+        if current_step <= warmup_steps:
+            return 1e-5 + (1.0 - 1e-5) * float(current_step) / float(
+                max(1, warmup_steps)
+            )
+        else:
+            remaining_steps = training_steps - warmup_steps
+            milestones = create_milestones(remaining_steps, m)
+            delta = 1 - (milestones[current_step - warmup_steps] / m)
+        return delta + end_lambda
+
+    return optim.lr_scheduler.LambdaLR(optimizer, lr_lambda, last_epoch)
 
 
 def get_optimizer(cfg: "DictConfig", model: "torch.nn.Module") -> "optim.Optimizer":
@@ -219,83 +301,27 @@ def get_optimizer(cfg: "DictConfig", model: "torch.nn.Module") -> "optim.Optimiz
     return optimizer
 
 
-def get_multistep_schedule_with_warmup(
-    optimizer: "optim.Optimizer",
-    warmup_steps: "int",
-    training_steps: "int",
-    end_lambda: "float" = 1e-3,
-    m: "int" = 10,
-    last_epoch: "int" = -1,
-) -> "optim.lr_scheduler.LambdaLR":
-    """
-    Modified schedule:
-      - Warmup: linearly increases from 0.001% of base LR to 100% of base LR over warmup_steps.
-      - Then, in the non-warmup phase (total_steps - warmup_steps):
-          * LR = base_lr * [(1- n/m) + end_lambda] where:
-              * m: number of milestones
-              * n: step number in [1, m]
-    """
-
-    def create_milestones(steps: "int", m: "int") -> "list[int]":
-        """returns a list of milestones for the given number of steps and m."""
-        g = int(steps // m)
-        milestones = []
-        for i in range(1, m + 1):
-            milestones += [i] * g
-        return milestones
-
-    def lr_lambda(current_step: int) -> "float":
-        """returns the learning rate multiplier based on the current step."""
-        if current_step <= warmup_steps:
-            return 1e-5 + (1.0 - 1e-5) * float(current_step) / float(
-                max(1, warmup_steps)
-            )
+def get_profiler(cfg: "DictConfig") -> "Profiler | None":
+    """Returns a suitable profiler for the used accelerator"""
+    if cfg.profiler:
+        if cfg.accelerator == "tpu":
+            profiler = XLAProfiler(port=cfg.profiler_port)
         else:
-            remaining_steps = training_steps - warmup_steps
-            milestones = create_milestones(remaining_steps, m)
-            delta = 1 - (milestones[current_step - warmup_steps] / m)
-        return delta + end_lambda
+            profiler = PyTorchProfiler(
+                filename=cfg.prof_filename, emit_nvtx=cfg.emit_nvtx
+            )
+    else:
+        profiler = None
+    return profiler
 
-    return optim.lr_scheduler.LambdaLR(optimizer, lr_lambda, last_epoch)
 
-
-def get_linear_schedule_with_warmup(
-    optimizer: "optim.Optimizer",
-    num_warmup_steps: "int",
-    num_training_steps: "int",
-    end_lambda: "float" = 0.0,
-    last_epoch: "int" = -1,
-) -> "optim.lr_scheduler.LambdaLR":
-    """
-    from https://github.com/huggingface/transformers/blob/main/src/transformers/optimization.py
-    Create a schedule with a learning rate that decreases linearly from the initial lr set in the optimizer to 0, after
-    a warmup period during which it increases linearly from 0 to the initial lr set in the optimizer.
-    Args:
-        optimizer ([`~torch.optim.Optimizer`]):
-            The optimizer for which to schedule the learning rate.
-        num_warmup_steps (`int`):
-            The number of steps for the warmup phase.
-        num_training_steps (`int`):
-            The total number of training steps.
-        end_lambda (`float`):
-            Mutiplicative factor at the end of training.
-        last_epoch (`int`, *optional*, defaults to -1):
-            The index of the last epoch when resuming training.
-    Return:
-        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
-    """
-
-    def lr_lambda(current_step: int) -> "float":
-        """returns the learning rate multiplier based on the current step."""
-        if current_step < num_warmup_steps:
-            return float(current_step) / float(max(1, num_warmup_steps))
-        return max(
-            end_lambda,
-            float(num_training_steps - current_step)
-            / float(max(1, num_training_steps - num_warmup_steps)),
-        )
-
-    return optim.lr_scheduler.LambdaLR(optimizer, lr_lambda, last_epoch)
+def get_seeded_generator(cfg: "DictConfig") -> "torch.Generator":
+    NP_MAX = np.iinfo(np.uint32).max
+    MAX_SEED = NP_MAX + 1
+    seed = cfg.seed % MAX_SEED
+    g = torch.Generator()
+    g.manual_seed(seed)
+    return g
 
 
 def get_scheduler(
@@ -358,22 +384,39 @@ def get_scheduler(
     return scheduler
 
 
-def get_profiler(cfg: "DictConfig") -> "Profiler | None":
-    """Returns a suitable profiler for the used accelerator"""
-    if cfg.profiler:
-        if cfg.accelerator == "tpu":
-            profiler = XLAProfiler(port=cfg.profiler_port)
-        else:
-            profiler = PyTorchProfiler(
-                filename=cfg.prof_filename, emit_nvtx=cfg.emit_nvtx
-            )
-    else:
-        profiler = None
-    return profiler
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
 
 
-def set_seed(seed: "int" = 57) -> None:
-    random.seed(seed)
+def set_seed(
+    seed: "int | None" = 4294967295,
+    cudnn_backend: "bool" = False,
+    use_deterministic_algorithms: "bool" = False,
+    warn_only: "bool" = True,
+) -> None:
     os.environ["PYTHONHASHSEED"] = str(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+
+    NP_MAX = np.iinfo(np.uint32).max
+    MAX_SEED = NP_MAX + 1
+
+    if seed is None:
+        seed_ = torch.default_generator.seed() % MAX_SEED
+        torch.manual_seed(seed_)
+    else:
+        seed = int(seed) % MAX_SEED
+        torch.manual_seed(seed)
+
+    random.seed(seed)
     np.random.seed(seed)
-    torch.manual_seed(seed)
+
+    if seed is not None and cudnn_backend:
+        torch.backends.cudnn.deterministic = True  # if True, causes cuDNN to only use deterministic convolution algorithms
+        torch.backends.cudnn.benchmark = False  # If True, causes cuDNN to benchmark multiple convolution algorithms and select the fastest
+
+    if use_deterministic_algorithms:
+        torch.use_deterministic_algorithms(
+            mode=use_deterministic_algorithms, warn_only=warn_only
+        )  # Sets whether PyTorch operations must use “deterministic” algorithms
